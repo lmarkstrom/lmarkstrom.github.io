8313b0144dd57cdae89d97ec4c83028c30c87006
Linus Markström <lmark@kth.se>
2024-04-29 01:33:28 +0200

    jsx and fix footer and header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef  } from 'react';
+import './Header.css';
+
+const Header = () => {
+    const [activeTab, setActiveTab] = useState(0);
+    const [underlineStyle, setUnderlineStyle] = useState({});
+    const tabsRef = useRef(null);
+
+    useEffect(() => {
+        const tabsContainerWidth = tabsRef.current.offsetWidth;
+        const tabWidth = tabsContainerWidth / 4;
+        setUnderlineStyle({
+            width: `${tabWidth}px`,
+            left: `${activeTab * tabWidth}px`
+        });
+    }, [activeTab]);
+
+    const handleTabClick = (index) => {
+        setActiveTab(index);
+        const tabsContainerWidth = document.querySelector('.tabs').offsetWidth;
+        const tabWidth = tabsContainerWidth / 4;
+        setUnderlineStyle({
+            width: `${tabWidth}px`,
+            left: `${index * tabWidth}px`
+        });
+    };
+
+    return (
+        <div className="header">
+            <div className="name">
+                
+            </div>
+            <div className='tabContainer' ref={tabsRef}>
+                <ul className="tabs">
+                    <li className={activeTab === 0 ? 'active' : ''} onClick={() => handleTabClick(0)}>About</li>
+                    <li className={activeTab === 1 ? 'active' : ''} onClick={() => handleTabClick(1)}>Projects</li>
+                    <li className={activeTab === 2 ? 'active' : ''} onClick={() => handleTabClick(2)}>Resume</li>
+                    <li className={activeTab === 3 ? 'active' : ''} onClick={() => handleTabClick(3)}>Contact</li>
+                    
+                </ul>
+                <div className="underline" style={underlineStyle}></div>
+            </div>
+        </div>
+    );
+    
+};
+
+export default Header
